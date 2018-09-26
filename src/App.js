902735767ed6dde2c235d7714c3fa34731b5e969
abisummers <hello@abisummers.com>
2018-09-26 10:19:23 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Recyle</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
