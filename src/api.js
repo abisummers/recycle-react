a4d0bf2ef5fa1d8d696bf027dda18be06c6d5fc1
Julie Ménard <menard_julie@ymail.com>
2018-09-27 12:34:27 +0200

    blah julie

---
+++ src/api.js
@@ -0,0 +1,9 @@
+import Axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:4000/api",
+    withCredentials: true,
+});
+
+
+export default api;
--- src/components/SearchBar/SearchResult.js
+++ src/components/SearchBar/SearchResult.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+
 function SearchResult(){
     return(
         <h1>blah</h1>
