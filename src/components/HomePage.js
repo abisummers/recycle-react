54245bc65675772ee2a9da53f79243f9e846985f
abisummers <hello@abisummers.com>
2018-09-28 15:52:01 +0200

    added general layout of home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,16 +4,35 @@ function HomePage(props) {
   const { currentUser } = props;
   return (
     <section>
-      <h2>Home</h2>
+      <div className="funFact">
+        <p>A fun fact</p>
+        <button>again</button>
+      </div>
       {currentUser && <p>Hi, {currentUser.fullName}</p>}
 
-      <div className="log-sign-form">
+      <div className="search-form">
         <form>
           <label>
-            <input />
+            <input type="text" placeholder="Search here..." />
           </label>
           <button>Search</button>
         </form>
+      </div>
+
+      <div className="row">
+        <button>PLASTIC</button>
+
+        <button>ELECTRONICS</button>
+
+        <button>COMPOST</button>
+      </div>
+
+      <div className="row">
+        <button>GLASS</button>
+
+        <button>PAPER</button>
+
+        <button>SEE ALL </button>
       </div>
     </section>
   );
