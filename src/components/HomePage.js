ff19342310c612860109dbc6e9430c17bde4aeac
abisummers <hello@abisummers.com>
2018-10-02 13:11:34 +0200

    Added fun fact in seperate component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,37 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar/SearchBar";
+import { Component } from "react";
+import FunFacts from "../components/FunFacts";
 
 function HomePage(props) {
   const { currentUser } = props;
   return (
     <section>
-      <div className="funFact">
-        <p>A fun fact</p>
-        <button>again</button>
-      </div>
       {currentUser && <p>Hi, {currentUser.fullName}</p>}
 
+      <FunFacts />
+
       <div className="search-form">
-       <SearchBar handleEvent={(inputValue)=> props.handleEvent(inputValue)}/>
+        <SearchBar handleEvent={inputValue => props.handleEvent(inputValue)} />
       </div>
 
       <div className="row">
-        <Link to="/material/plastiques" className="plastic">PLASTIQUES</Link>
+        <Link to="/material/plastiques" className="plastic">
+          PLASTIQUES
+        </Link>
 
-        <Link to="/material/dechets-electriques-et-electroniques" className="electriques">
+        <Link
+          to="/material/dechets-electriques-et-electroniques"
+          className="electriques"
+        >
           ELECTRIQUES & ELECTRONIQUES
         </Link>
 
-        <Link to="/material/dechets-de-cuisine" className="compost">COMPOST</Link>
+        <Link to="/material/dechets-de-cuisine" className="compost">
+          COMPOST
+        </Link>
       </div>
 
       <div className="row">
-        <Link to="/material/verre" className="verre">VERRE</Link>
+        <Link to="/material/verre" className="verre">
+          VERRE
+        </Link>
 
-        <Link to="/material/papiers-cartons" className="papier">PAPIER</Link>
+        <Link to="/material/papiers-cartons" className="papier">
+          PAPIER
+        </Link>
 
-        <Link to="/all-categories" className="toutes-categories">VOIR TOUTES LES CATEGORIES</Link>
+        <Link to="/all-categories" className="toutes-categories">
+          VOIR TOUTES LES CATEGORIES
+        </Link>
       </div>
     </section>
   );
