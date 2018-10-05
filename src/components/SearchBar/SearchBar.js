451de718701360ec80eed0dac53351b6f07020b3
abisummers <hello@abisummers.com>
2018-10-05 11:29:03 +0200

    imported withRouter

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 class SearchBar extends React.Component {
   constructor(props) {
