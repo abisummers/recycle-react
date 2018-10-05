a68926839f602d446b3a1ff134bc5a4c7b8f43ba
Julie Ménard <menard_julie@ymail.com>
2018-10-05 14:06:32 +0200

    finishd list

--- src/components/Categories/CategoryPopup.js
+++ src/components/Categories/CategoryPopup.js
@@ -11,7 +11,7 @@ const CategoryPopup = (props) => {
                     <React.Fragment>
                         <h3>{props.catToShow.fields.produits}</h3>
                         <p><b>Type:</b> {props.catToShow.fields.typologie_des_dechets}</p>
-                        <p><b>Qu'est-ce que j'en fais ? :</b>{" "}{props.catToShow.fields.qu_est_ce_que_j_en_fais}</p>
+                        <p className="cadre-vert"><b>Qu'est-ce que j'en fais ? :</b>{" "}{props.catToShow.fields.qu_est_ce_que_j_en_fais}</p>
                         <p><b>Que va-t-il devenir ? :</b>{" "}{props.catToShow.fields.que_va_t_il_devenir}</p>
 
                     </React.Fragment>
